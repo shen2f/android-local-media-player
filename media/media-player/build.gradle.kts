e11904157ae21d5f41ae83edc472f0129aac5639
shen2f <shen.zhongfu@163.com>
2026-03-08 17:34:34 +0800

    media-player gradle dependencies

--- media/media-player/build.gradle.kts
+++ media/media-player/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -26,7 +28,15 @@ android {
 dependencies {
     implementation(project(":core:core-common"))
     implementation(project(":media:media-decoder"))
-    
+    implementation("androidx.media3:media3-session:1.2.0")
+
+    // Kotlin协程
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    // Hilt依赖注入
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.media3:media3-exoplayer:1.2.0")
     implementation("androidx.media3:media3-ui:1.2.0")
